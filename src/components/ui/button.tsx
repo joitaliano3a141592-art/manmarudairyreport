4f7d83041c4ad8345b04aa4135df8cacb08a60e2
suzuki <aiueo>
2026-04-16 11:28:42 +0900

    Default button type to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -42,6 +42,7 @@ function Button({
   variant,
   size,
   asChild = false,
+  type,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
@@ -53,6 +54,7 @@ function Button({
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      type={asChild ? undefined : (type ?? "button")}
       {...props}
     />
   )
